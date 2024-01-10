0ab22b27c1b3f3918626704b2052a687e434d374
Chandler Hardy <hardych04@gmail.com>
2024-01-10 09:51:03 -0600

    Added media queries for smaller screens

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -4,74 +4,97 @@ import LogoS from '../../assets/images/logo-s.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
+  faBars,
+  faClose,
   faEnvelope,
   faHome,
   faSuitcase,
   faUser,
 } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/react-portfolio/">
-      <img src={LogoS} alt="logo" />
-      {/* <img className="sub-logo" src={LogoSubtitle} alt="chandler" /> */}
-    </Link>
-    <nav>
-      {/* Home */}
-      <NavLink exact="true" activeclassname="active" to="/react-portfolio/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      {/* About */}
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/react-portfolio/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      {/* Portfolio */}
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="portfolio-link"
-        to="/react-portfolio/portfolio"
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-      </NavLink>
-      {/* Contact */}
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/react-portfolio/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    {/* Social Links */}
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/chandler-hardy-80808112b/"
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/react-portfolio/">
+        <img src={LogoS} alt="logo" />
+      </Link>
+      <nav className={` ${showNav ? 'mobile-show' : ''}`}>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeClassName="active"
+          to="/react-portfolio/"
         >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.github.com/chandlerhardy"
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeClassName="active"
+          className="about-link"
+          to="/react-portfolio/about"
         >
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeClassName="active"
+          className="portfolio-link"
+          to="/react-portfolio/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeClassName="active"
+          className="contact-link"
+          to="/react-portfolio/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/chandler-hardy-80808112b/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.github.com/chandlerhardy"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  )
+}
 
 export default Sidebar
